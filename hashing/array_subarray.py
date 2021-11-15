d9d7a36000efb4c45f51bb01ca61b6faacc2897f
Praveen Bommali <65863196+praveenbommalibits@users.noreply.github.com>
2021-11-15 07:35:41 +0530

    hashing problem solving

--- hashing/array_subarray.py
+++ hashing/array_subarray.py
@@ -1,15 +1,46 @@
 # Task: find ab array is subset of another array
 
 
-def quick_sort(arr1, si, ei):
+def partition(arr, si, ei):
+    x = arr[ei]
+    i = (si - 1)
+
+    for j in range(si, ei):
+        if arr[j] <= x:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i + 1], arr[ei] = arr[ei], arr[i + 1]
+    return i + 1
+
+
+def quick_sort(arr, si, ei):
     # partitioning index
     if si < ei:
-        pass
+        pi = partition(arr, si, ei)
+        quick_sort(arr, si, pi - 1)
+        quick_sort(arr, pi + 1, ei)
+
+
+def binary_search(arr, low, high, x):
+    if high >= low:
+        mid = (low + high) // 2
+        if (mid == 0 or x > arr[mid - 1]) and (arr[mid] == x):
+            return mid
+        elif x > arr[mid]:
+            return binary_search(arr, mid + 1, high, x)
+        else:
+            return binary_search(arr, low, mid - 1, x)
+    return -1
 
 
 def is_subset(arr1, arr2, m, n):
     i = 0
     quick_sort(arr1, 0, m - 1)
+    for i in range(n):
+        if binary_search(arr1, 0, m - 1, arr2[i]) == -1:
+            return 0
+
+    return 1
 
 
 arr1 = [11, 1, 13, 21, 3, 7]
